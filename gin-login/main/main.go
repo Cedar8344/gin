b98bea248dafa448a8cdd5c3842c087976aa4a99
Cedar8344 <jerryang1023@gmail.com>
2020-07-20 17:55:55 -0700

    First

---
+++ gin-login/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"gin-login/mappings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	mappings.CreateUrlMappings()
+	// Listen and server on 0.0.0.0:8080
+	mappings.Router.Run(":8080")
+
+}
